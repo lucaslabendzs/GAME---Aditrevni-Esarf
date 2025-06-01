a4dec4dcb63e289dd07cf21ac7530b4e994be0eb
Kauan Calegari <kauancalegari@ufpr.br>
2025-06-01 19:44:53 +0000

    Upload New File

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang=br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Aditrevni arvralap</h1>
+</body>
+</html>
