b4d4294beeaf384bd3d8b0dea388b4fd41f732d6
Renan teles <renantelesdeoliveira@gmail.com>
2025-06-02 20:00:25 -0300

    U

---
+++ paginaInicial.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet">
+    <title>Home page</title>
+</head>
+<body>
+    <header class="titulo">
+        <h1>Aditrevni arvralap</h1>
+    </header>
+    <div class="regras">
+        <label for="regras">Regras do Jogo:</label>
+        <textarea id="regras" name="regras" rows="10" cols="50" readonly>
+            1. Quanto mais rápido você responder, maior vai ser a pontuação.
+            2. Cada se a resposta estiver errado, você perde um ponto.
+            3. O jogo termina quando todas as palavras forem escritas.
+            4. Não é permitido usar consultas externas ou colar respostas.
+            5. Divirta-se e desafie seus amigos para ver quem faz mais pontos!
+            6. Pontuação e semanal, todas as segundas-feiras a pontuação e resetada.
+        </textarea>
+    </div>
+
+    <div class="botao-pag">
+        <input type="submit" value="Iniciar" id="botao-hom" />
+    </div>
+     <footer class="rodape">
+        <p>© 2025 Kauan Calegari, Lucas Labendzs, Renan Teles. Todos os direitos reservados.</P>
+    </footer>
+    
+</body>
+</html>
