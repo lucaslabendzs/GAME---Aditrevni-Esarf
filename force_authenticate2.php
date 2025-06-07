b98196e7bf6984c0d2ebd2185bc7ebd0ccf38eb8
Lucas Labendzs <lucaslabendzs@ufpr.br>
2025-06-07 20:40:54 -0300

    css do logado

--- force_authenticate2.php
+++ force_authenticate2.php
@@ -1,14 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="logado.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
 
-  require "authenticate.php";
+<body>
+  <div class="login-container">
+    <?php
+    require "authenticate.php";
 
-  if($login){
-    die("Você já está logado!!
-    <ul>
-      <li><a href='paginaInicial.php'>Voltar</a></li>
-    </ul>
-    ");
-  }
-
-  
-?>
+    if ($login) {
+      die("
+  <h2>Você já está logado!!</h2>
+  <a href='paginaInicial.php' class='button-link'>Voltar</a>
+");
+    }
+    ?>
+  </div>
+</body>
+</html>
