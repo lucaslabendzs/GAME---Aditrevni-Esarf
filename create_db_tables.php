fb88c033c83210e799e8141e7041a39882a9787d
Kauan Calegari <kauancalegari@ufpr.br>
2025-06-12 18:29:19 +0000

    Edit create_db_tables.php

--- create_db_tables.php
+++ create_db_tables.php
@@ -1,27 +1,27 @@
 <?php
 require 'db_credentials.php';
 
-// Create connection
+// Criando conexão
 $conn = mysqli_connect($servername, $username, $db_password);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Check connection
+// check conexão
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Create database
-$sql = "CREATE DATABASE $dbname";
+// Ciando BD
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
 if (mysqli_query($conn, $sql)) {
     echo "<br>Database created successfully<br>";
 } else {
     echo "<br>Error creating database: " . mysqli_error($conn);
 }
 
-// Choose database
+// Escolhendo o BD
 $sql = "USE $dbname";
 if (mysqli_query($conn, $sql)) {
     echo "<br>Database changed successfully<br>";
@@ -29,8 +29,8 @@ if (mysqli_query($conn, $sql)) {
     echo "<br>Error changing database: " . mysqli_error($conn);
 }
 
-// sql to create table
-$sql = "CREATE TABLE $table_users (
+// sql to create table user
+$sql = "CREATE TABLE IF NOT EXISTS $table_users (
   id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
@@ -42,10 +42,27 @@ $sql = "CREATE TABLE $table_users (
   UNIQUE (email)
 )";
 
+
+
 if (mysqli_query($conn, $sql)) {
     echo "<br>Table created successfully<br>";
 } else {
     echo "<br>Error creating database: " . mysqli_error($conn);
+}
+
+// sql to create table rankingDiario
+$sql_ranking = "CREATE TABLE IF NOT EXISTS $table_ranking_diario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(6) UNSIGNED NOT NULL,
+    tempo_segundos  DECIMAL(8,2) NOT NULL,
+    data_jogo DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+)";
+
+if (mysqli_query($conn, $sql_ranking)) {
+    echo "<br>Table RankingDiario created successfully<br>";
+} else {
+    echo "<br>Error creating RankingDiario: " . mysqli_error($conn);
 }
 
 mysqli_close($conn)
