7ce279a9463917a01ae39baabdc4692dbe89157a
Kauan Calegari <kauancalegari@ufpr.br>
2025-06-12 18:36:57 +0000

    Add new file

---
+++ ja_jogou.php
@@ -0,0 +1,15 @@
+<?php
+require "db_credentials.php";
+require "force_authenticate.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Já jogou</title>
+</head>
+<body>
+    <p>ja jogou hoje</p>
+</body>
+</html>
